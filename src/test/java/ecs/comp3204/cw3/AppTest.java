e22e8eecaf9b192e6c8fcf2b099c47fd8ac427c5
Callum Connolly <callumc11@gmail.com>
2019-12-08 14:39:13 +0000

    Initial Commit

---
+++ src/test/java/ecs/comp3204/cw3/AppTest.java
@@ -0,0 +1,18 @@
+package ecs.comp3204.cw3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigourous Test :-)
+     */
+	@Test
+    public void testApp() {
+        assertTrue( true );
+    }
+}
