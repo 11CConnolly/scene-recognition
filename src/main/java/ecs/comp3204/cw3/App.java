0f3a9fb3b5e5eb0a68405ea75a08071d00f21a73
JackeyLiuSoton <khl1g17@soton.ac.uk>
2019-12-15 05:32:04 +0000

    updated the App.java to use the hello world template to describe our team

--- src/main/java/ecs/comp3204/cw3/App.java
+++ src/main/java/ecs/comp3204/cw3/App.java
@@ -14,16 +14,19 @@ import org.openimaj.image.typography.hershey.HersheyFont;
 public class App {
     public static void main( String[] args ) {
     	//Create an image
-        MBFImage image = new MBFImage(320,70, ColourSpace.RGB);
+        MBFImage image = new MBFImage(888,200, ColourSpace.RGB);
 
         //Fill the image with white
-        image.fill(RGBColour.WHITE);
+        image.fill(RGBColour.BLACK);
         		        
         //Render some test into the image
-        image.drawText("Hello World", 10, 60, HersheyFont.CURSIVE, 50, RGBColour.BLACK);
+        image.drawText("Hello World! We are Team 33!", 10, 60, HersheyFont.TIMES_BOLD, 50, RGBColour.RED);
+        image.drawText("Callum Connolly", 10, 140, HersheyFont.CURSIVE, 30, RGBColour.RED);
+        image.drawText("Kwok Hung Liu", 10, 170, HersheyFont.CURSIVE, 30, RGBColour.RED);
+        image.drawText("Peter Deng", 10, 200, HersheyFont.CURSIVE, 30, RGBColour.RED);
 
-        //Apply a Gaussian blur
-        image.processInplace(new FGaussianConvolve(2f));
+        //Apply a Gaussian blur - nah
+        image.processInplace(new FGaussianConvolve(0f));
         
         //Display the image
         DisplayUtilities.display(image);
